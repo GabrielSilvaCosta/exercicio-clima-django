67e3d7bf2d9d0603698e7c56207b8ba970e4b01a
GabrielSilvaCosta <bob@>
2024-01-16 14:41:41 -0300

    requisto 6 feito

--- weather/urls.py
+++ weather/urls.py
@@ -6,5 +6,9 @@ from weather.views import index, city_weather, weather_details
 urlpatterns = [
     path("", index, name="home-page"),
     path("weather/<str:city>", city_weather, name="city-page"),
-    path("weather/<str:city>/<str:target>", weather_details, name="weather"),
+    path(
+        "weather/<str:city>/<str:target>",
+        weather_details,
+        name="weather-details-page",
+    ),
 ]
