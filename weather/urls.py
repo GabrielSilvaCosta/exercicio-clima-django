81a119fa29409733f049ed575b5ba6925195c83a
GabrielSilvaCosta <bob@>
2024-01-16 11:22:18 -0300

    requisito 4 feito

--- weather/urls.py
+++ weather/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
-from weather.views import index
+# Arquivo: weather/urls.py
 
+from django.urls import path
+from weather.views import index, city_weather
 
 urlpatterns = [
     path("", index, name="home-page"),
+    path("weather/<str:city>", city_weather, name="city-page"),
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,9 +1,19 @@
 # Arquivo: weather/views.py
 
 from django.shortcuts import render
-from weather.models import City
+from weather.models import City, DailyWeather
 
 
 def index(request):
     cities = {"cities": City.objects.all()}
     return render(request, "home.html", cities)
+
+
+def city_weather(request, city):
+    city_name = city.replace("-", " ").title()
+
+    queried_city = City.objects.get(name=city_name)
+    weathers = DailyWeather.objects.filter(city=queried_city)
+
+    context = {"city": queried_city, "weathers": weathers}
+    return render(request, "city_weather.html", context)
