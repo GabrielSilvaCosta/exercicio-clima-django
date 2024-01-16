bd6df5a01231a2c15cb021c6668f54fcd7199622
GabrielSilvaCosta <bob@>
2024-01-16 09:40:54 -0300

    requisito 1 feito

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,20 @@
+# Arquivo: weather/models.py
+
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=60)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+    def slugify(self):
+        lower_name = self.name.replace(" ", "-").lower()
+        return lower_name
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.title()
+        super(City, self).save(*args, **kwargs)
